fbaec445dc90b149dca040127701566427f1fa5b
adriagc8 <adrigonzalezcopado@gmail.com>
2022-03-30 20:47:58 +0200

    history base tab and poll component

--- interfaces/index.tsx
+++ interfaces/index.tsx
@@ -7,10 +7,14 @@ export interface Poll {
   result: Result
   qty: number
   addr: string
+  // state: string // pending, approved & declined
 }
 
 export interface Result {
   yes: number
   no: number
   abs: number
+}
+export interface Category {
+  category: Array<Poll>,
 }
---
+++ pages/history/ClosedPoll.tsx
@@ -0,0 +1,10 @@
+import Poll from '../../components/Poll'
+
+const ClosedPoll = (props: any) => {
+  return <Poll {...props}>
+
+  </Poll>
+}
+
+export default ClosedPoll
+
