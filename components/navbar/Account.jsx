019afd4fd3be1b60cca5bdbf4f07702443ef477c
Víctor Barberà <vcitor.bq@gmail.com>
2022-04-05 19:41:22 +0200

    prepareed to merge

--- components/navbar/Account.jsx
+++ components/navbar/Account.jsx
@@ -1,39 +1,7 @@
 import { useMoralis } from 'react-moralis'
-const styles = {
-  account: {
-    height: '42px',
-    padding: '0 15px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 'fit-content',
-    borderRadius: '12px',
-    backgroundColor: 'rgb(244, 244, 244)',
-    cursor: 'pointer',
-  },
-  text: {
-    color: '#21BF96',
-  },
-  connector: {
-    alignItems: 'center',
-    display: 'flex',
-    flexDirection: 'column',
-    height: 'auto',
-    justifyContent: 'center',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    padding: '20px 5px',
-    cursor: 'pointer',
-  },
-  icon: {
-    alignSelf: 'center',
-    fill: 'rgb(40, 13, 95)',
-    flexShrink: '0',
-    marginBottom: '8px',
-    height: '30px',
-  },
-}
-function Account() {
+import Image from 'next/image'
+
+const Account = () => {
   const { authenticate, isAuthenticated, account, logout } = useMoralis()
   const handleAuthenticateClick = () =>
     authenticate({ signingMessage: 'Login with you metamask wallet' })
@@ -41,22 +9,20 @@ function Account() {
 
   if (!isAuthenticated || !account) {
     return (
-      <>
-        <button
-          className="flex basis-1/3 items-center justify-end"
-          style={styles.text}
-          onClick={handleAuthenticateClick}
-        >
-          <p className="w-full items-center justify-center rounded px-3 py-2 font-bold text-white hover:bg-slate-700 hover:text-white lg:mx-6 lg:inline-flex lg:w-auto">
-            Authenticate
-          </p>
-          <img
-            src="./metamaskWallet.png"
-            alt="Authenticate with Metamask"
-            style={styles.icon}
-          />
-        </button>
-      </>
+      <button
+        className="flex basis-1/3 items-center justify-end"
+        onClick={handleAuthenticateClick}
+      >
+        <Image
+          width="30"
+          height="30"
+          src="/metamask.png"
+          alt="Metamask icon."
+        />
+        <p className="w-full items-center justify-center rounded px-3 py-2 font-bold text-white hover:bg-slate-700 hover:text-white lg:mx-6 lg:inline-flex lg:w-auto">
+          Authenticate
+        </p>
+      </button>
     )
   } else {
     return (
