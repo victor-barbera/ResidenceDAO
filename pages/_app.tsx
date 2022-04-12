9711a98df0713a8c0fbba43505750f291dc9fcda
Víctor Barberà <vcitor.bq@gmail.com>
2022-04-13 00:34:19 +0200

    moralis a environment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,10 @@ import { MoralisProvider } from 'react-moralis'
 import { Toaster } from 'react-hot-toast'
 import NavBar from '../components/NavBar'
 
-// TODO: Enviar això a .env
-const APP_ID = 'vxRfT23BgKAB7xdJDmaqwOJRwnC9YmdCGFK9A6Fw'
-const SERVER_URL = 'https://ss5pmkqpc8n0.usemoralis.com:2053/server'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // ! El problema és que això s'executa masses cops
-  // if (!APP_ID || !SERVER_URL)
-  //   throw new Error(
-  //     'Missing Moralis Application ID or Server URL. Make sure to set your .env file.'
-  //   );
   return (
-    <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+    <MoralisProvider appId={process.env.NEXT_PUBLIC_MORALIS_APPLICATION_ID!} serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER_URL!}>
       <div className="flex flex-col bg-slate-800 min-h-screen">
         <NavBar />
         <Toaster containerStyle={{ top: 80 }} />
