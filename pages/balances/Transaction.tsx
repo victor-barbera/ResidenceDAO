5fcc4598aba537d913250c27afd754f3c016d0c2
adriagc8 <adrigonzalezcopado@gmail.com>
2022-04-19 18:08:05 +0200

    short and long wallets okai

--- pages/balances/Transaction.tsx
+++ pages/balances/Transaction.tsx
@@ -8,6 +8,7 @@ const TransactionTr =  (props: any) => {
   const [direction, setDirection] = useState("")
   const [directionIcon, setDirectionIcon] = useState("")
   const [wallet, setWallet] = useState("")
+  const [shortWallet, setShortWallet] = useState("")
 
   useEffect(()=>{
 
@@ -26,17 +27,19 @@ const TransactionTr =  (props: any) => {
       // you are the sender
       setDirection("sent");
       setDirectionIcon("M12 7a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0V8.414l-4.293 4.293a1 1 0 01-1.414 0L8 10.414l-4.293 4.293a1 1 0 01-1.414-1.414l5-5a1 1 0 011.414 0L11 10.586 14.586 7H12z");
-      setWallet(addrShortener(props.to));
+      setWallet(props.to);
+      setShortWallet(addrShortener(props.to))
     }else{
       setDirection("received")
       setDirectionIcon("M12 13a1 1 0 100 2h5a1 1 0 001-1V9a1 1 0 10-2 0v2.586l-4.293-4.293a1 1 0 00-1.414 0L8 9.586 3.707 5.293a1 1 0 00-1.414 1.414l5 5a1 1 0 001.414 0L11 9.414 14.586 13H12z")
-      setWallet(addrShortener(props.from));
+      setWallet(props.from);
+      setShortWallet(addrShortener(props.from))
 
     }
   },[props.state, props.from, props.to ])
 
   const copyToClipboard = ()=> {
-    navigator.clipboard.writeText(props.addr)
+    navigator.clipboard.writeText(wallet)
     toast.success("Copied to clipboard!")
   }
   const addrShortener = (addr: string) => `${addr.slice(0, 6)}...${addr.slice(-4)}`
@@ -52,7 +55,7 @@ const TransactionTr =  (props: any) => {
 
             </div>
             <div className="flex flex-row">
-                <p className="basis-3/4 ml-11 text-left" > {wallet}</p>
+                <p className="basis-3/4 ml-11 text-left" > {shortWallet}</p>
                 <button onClick={copyToClipboard}><ClipboardIcon className='basis-1/4 w-5 h-5 text-slate-500 hover:text-slate-300' /></button>
 
             </div>
