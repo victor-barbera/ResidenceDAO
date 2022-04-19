3bed1cbb63a398fe262e531766074365e3e2847f
adriagc8 <adrigonzalezcopado@gmail.com>
2022-04-19 10:50:14 +0200

    maquetacio balances completa

--- pages/balances/TransactionsTable.tsx
+++ pages/balances/TransactionsTable.tsx
@@ -1,41 +1,86 @@
-const Transactions = () => {
-    return (
-<div class="relative  bg-slate-900 px-10 py-10 my-8 overflow-x-auto shadow-md sm:rounded-lg">
-<h1 className=" text-2xl font-semibold text-white antialiased">
-    TRANSACTIONS
-</h1>
-    <table className="table-fixed w-full bg-slate-800 text-slate-400 rounded-lg  mt-8 py-5 px-5">
+const Transactions =  (props: any) => {
+  return (
+    <div class="relative  my-8 overflow-x-auto bg-slate-900 px-10 py-10 shadow-md sm:rounded-lg">
+      <h1 className=" text-2xl font-semibold text-white antialiased">
+        TRANSACTIONS
+      </h1>
+      <table className="mt-8 w-full table-fixed rounded-lg bg-slate-800  py-5 px-5 text-slate-400">
         <thead>
-            <tr>
-                <th className="font-medium p-4  text-slate-400  text-center">Description</th> 
-                <th className="font-medium p-4  text-slate-400  text-center">Amount</th> 
-                <th className="font-medium p-4  text-slate-400  text-center">Date</th> 
-                <th className="font-medium p-4  text-slate-400  text-center">State</th> 
-            </tr> 
-        </thead> 
-        <tbody >
-            {/* className="bg-slate-800 dark:bg-slate-900">  */}
-            <tr  className="border-b bg-slate-700 border-slate-900"> 
-                <td className="text-slate-300 text-center px-5 py-4">Tokens sent  0x08AG.....8877a</td> 
-                <td className="text-slate-300 text-center px-5 py-4">80 $</td> 
-                <td className="text-slate-300 text-center px-5 py-4">2 Apr 2022 2:00</td> 
-                <td className="text-slate-300 text-center px-5 py-4">Pending</td> 
-            </tr> 
-            <tr  className="border-b bg-slate-700 border-slate-900"> 
-                <td className="text-slate-300 text-center px-5 py-4">Tokens sent  0x08AG.....8877a</td> 
-                <td className="text-slate-300 text-center px-5 py-4">80 $</td> 
-                <td className="text-slate-300 text-center px-5 py-4">2 Apr 2022 2:00</td> 
-                <td className="text-slate-300 text-center px-5 py-4">Pending</td> 
-            </tr>             <tr  className="border-b bg-slate-700 border-slate-900"> 
-                <td className="text-slate-300 text-center px-5 py-4">Tokens sent  0x08AG.....8877a</td> 
-                <td className="text-slate-300 text-center px-5 py-4">80 $</td> 
-                <td className="text-slate-300 text-center px-5 py-4">2 Apr 2022 2:00</td> 
-                <td className="text-slate-300 text-center px-5 py-4">Pending</td> 
-            </tr> 
-
-        </tbody> 
-    </table>
+          <tr>
+            <th className="p-4 text-center  font-medium  text-slate-400">
+              Description
+            </th>
+            <th className="p-4 text-center  font-medium  text-slate-400">
+              Amount
+            </th>
+            <th className="p-4 text-center  font-medium  text-slate-400">
+              Date
+            </th>
+            <th className="p-4 text-center  font-medium  text-slate-400">
+              State
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* className="bg-slate-800 dark:bg-slate-900">  */}
+          <tr className="border-b border-slate-900 bg-slate-700">
+            <td className="px-5 py-4 text-center text-slate-300">
+               <div className="flex flex-row gap-4">
+<svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z" clipRule="evenodd" /></svg>                    <p className="basis-3/4">Tokens sent 0x08AG.....8877a</p>
+               </div>
+            </td>
+            <td className="px-5 py-4 text-center text-slate-300"> - 80 $</td>
+            <td className="px-5 py-4 text-center text-slate-300">
+              2 Apr 2022 2:00
+            </td>
+            <td>
+              <a target="_blank" href="https://polygonscan.com/tx/0x64d4b51ba78bfdea503ed861908d62b2bdc498b15250a90b9ccf8be095ce7b08">
+                  <div className="flex flex-row  p-1  mx-10 bg-slate-800 shadow-md sm:rounded">
+                    <p className="basis-3/4 text-center text-slate-300">Pending</p>
+                    <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd" /></svg>
+                  </div>
+              </a>
+            </td>
+          </tr>
+          <tr className="border-b border-slate-900 bg-slate-700">
+          <td className="px-5 py-4 text-center text-slate-300">
+               <div className="flex flex-row gap-4">
+               <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12 13a1 1 0 100 2h5a1 1 0 001-1V9a1 1 0 10-2 0v2.586l-4.293-4.293a1 1 0 00-1.414 0L8 9.586 3.707 5.293a1 1 0 00-1.414 1.414l5 5a1 1 0 001.414 0L11 9.414 14.586 13H12z" clipRule="evenodd" /></svg>                    
+               <p className="basis-2/3">Tokens received 0x08AG.....8877a</p>
+               </div>
+            </td>
+            <td className="px-5 py-4 text-center text-slate-300"> + 80 $</td>
+            <td className="px-5 py-4 text-center text-slate-300">
+              2 Apr 2022 2:00
+            </td>
+            <td>
+            <a target="_blank" href="https://polygonscan.com/tx/0x64d4b51ba78bfdea503ed861908d62b2bdc498b15250a90b9ccf8be095ce7b08">
+                  <div className="flex flex-row  p-1  mx-10 bg-slate-800 shadow-md sm:rounded">
+                    <p className="basis-3/4 text-center text-slate-300">Confirmed</p>
+                    <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" /></svg>                    </div>
+              </a>
+            </td>          </tr>{' '}
+          <tr className="border-b border-slate-900 bg-slate-700">
+          <td className="px-5 py-4 text-center text-slate-300">
+               <div className="flex flex-row gap-4">
+                    <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12 7a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0V8.414l-4.293 4.293a1 1 0 01-1.414 0L8 10.414l-4.293 4.293a1 1 0 01-1.414-1.414l5-5a1 1 0 011.414 0L11 10.586 14.586 7H12z" clipRule="evenodd" /></svg>
+                    <p className="basis-3/4">Tokens sent 0x08AG.....8877a</p>
+               </div>
+            </td>
+            <td className="px-5 py-4 text-center text-slate-300">- 80 $</td>
+            <td className="px-5 py-4 text-center text-slate-300">
+              2 Apr 2022 2:00
+            </td>
+            <td>
+            <a target="_blank" href="https://polygonscan.com/tx/0x64d4b51ba78bfdea503ed861908d62b2bdc498b15250a90b9ccf8be095ce7b08">
+                  <div className="flex flex-row  p-1  mx-10 bg-slate-800 shadow-md sm:rounded">
+                    <p className="basis-3/4 text-center text-slate-300">Failed</p>
+                    <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" /></svg>                  </div>
+              </a>
+            </td>          </tr>
+        </tbody>
+      </table>
     </div>
-
-    )} 
-    export default Transactions
+  )
+}
+export default Transactions
--- pages/balances/index.tsx
+++ pages/balances/index.tsx
@@ -30,9 +30,12 @@ const Balances: NextPage = () => {
           <p className="text-2l mx-5 mt-5 bg-slate-900 text-center font-semibold text-white antialiased">
             Your $Coin balance
           </p>
-          <p className="text-center text-2xl font-semibold text-white antialiased">
-            100000
+          <div className="mx-10 mt-3  bg-slate-800  shadow-md sm:rounded-lg">
+          <p className="text-center p-2 text-2xl font-semibold text-white antialiased">
+            {DUMMY_BALANCE}
           </p>
+          </div>
+
         </div>
         <div className="basis-3/4 overflow-x-auto bg-slate-900 shadow-md sm:rounded-lg ">
           <div className="text-2l mx-10 mt-5 bg-slate-900 text-left font-semibold text-white antialiased">
@@ -56,7 +59,7 @@ const Balances: NextPage = () => {
                 />
               {/* <div className="  "> */}
                 <input 
-                  className="mx-10 my-3 basis-1/3 p-2 text-slate-400"
+                  className="mx-10 my-3 basis-1/3 p-2 text-slate-400  bg-slate-800  shadow-md sm:rounded-lg"
                   type="submit"
                   value="Send" />
                 {/* <RoundButton
@@ -69,9 +72,47 @@ const Balances: NextPage = () => {
           {/* </div> */}
         </div>
       </div>
-      <TransactionsTable />
+      <TransactionsTable  />
     </>
   )
 }
 
+const COLUMNS_EVENTS_TRANSFER: Array<Poll> = [
+  'Description',
+  'Amount',
+  'Date',
+  'State'
+]
+
+// Dades ja transformades desde front.
+const DUMMY_EVENTS_TRANSFER: Array<Poll> = [
+  {
+    id: 1,
+    state: 'pending',
+    txHash: '0x64d4b51ba78bfdea503ed861908d62b2bdc498b15250a90b9ccf8be095ce7b08',
+    from: '0xF9F613BDec2703ede176cC98A2276fA1F618A1b1',
+    to: '0x0000000000000000000000000000000000000000',
+    value: '1000',
+    date: 'Thu, 14 Apr 2022 15:45:30 GMT'
+  },
+  {
+    id: 2,
+    state: 'confirmed',
+    txHash: '0x64d4b51ba78bfdea503ed861908d62b2bdc498b15250a90b9ccf8be095ce7b08',
+    from: '0x0000000000000000000000000000000000000000',
+    to: '0xF9F613BDec2703ede176cC98A2276fA1F618A1b1',
+    value: '100',
+    date: 'Thu, 14 Apr 2022 11:45:30 GMT'
+  },
+  {
+    id: 3,
+    state: 'failed',
+    txHash: '0x64d4b51ba78bfdea503ed861908d62b2bdc498b15250a90b9ccf8be095ce7b08',
+    from: '0x0000000000000000000000000000000000000000',
+    to: '0xF9F613BDec2703ede176cC98A2276fA1F618A1b1',
+    value: '100',
+    date: 'Thu, 14 Apr 2022 09:45:30 GMT'
+  },
+]
+const DUMMY_BALANCE : Number = 100000;
 export default Balances
