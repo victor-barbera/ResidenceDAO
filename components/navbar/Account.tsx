4b0a9c28c02a83a90821b9cb56481cc14a9f3958
Víctor Barberà <vcitor.bq@gmail.com>
2022-05-05 18:43:58 +0200

    logout button created and styled

--- components/navbar/Account.tsx
+++ components/navbar/Account.tsx
@@ -1,10 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect, Fragment } from 'react'
 import Image from 'next/image'
 import { useMoralis } from 'react-moralis'
 import MoralisType from "moralis";
-import { Menu } from '@headlessui/react'
+import { Menu, Transition } from '@headlessui/react'
 import RoundButton from '../RoundButton'
 import RoundBlockie from '../RoundBlockie'
+import {LogoutIcon} from '@heroicons/react/outline'
+
 
 const Account = () => {
   const { authenticate, isAuthenticated, account, logout, isWeb3Enabled, isWeb3EnableLoading, enableWeb3 } = useMoralis()
@@ -27,10 +29,40 @@ const Account = () => {
       <p className="ml-2 font-mono">Authenticate</p>
     </RoundButton>
   ) : (
-    <div className="ml-auto mr-3 flex items-center rounded bg-slate-800 py-2 px-3 ring ring-slate-700">
-      <RoundBlockie addr={account} />
-      <p className="ml-3 font-mono text-slate-500">{addrShortener(account)}</p>
-    </div>
+    <Menu as="div" className="ml-auto">
+      <Menu.Button>
+        <div className="mr-3 flex items-center rounded bg-slate-800 py-2 px-3 ring ring-slate-700">
+          <RoundBlockie addr={account} />
+          <p className="ml-3 font-mono text-slate-500">{addrShortener(account)}</p>
+        </div>
+      </Menu.Button>
+      <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+      <Menu.Items className="absolute mt-2">
+        <Menu.Item>
+          {({ active }) => (
+            <button
+            onClick={handleLogoutClick}
+              className={`${
+                active ? 'bg-purple-500 text-white' : 'bg-slate-600 text-white'
+              } flex items-center w-full rounded-md text-lg px-3 py-2 mr-2`}
+            >
+              <LogoutIcon className="w-7 h-7 mr-2 text-red-600" />
+              Logout
+            </button>
+          )}
+        </Menu.Item>
+        {/* ... */}
+      </Menu.Items>
+      </Transition>
+    </Menu>
   )
 }
 export default Account
